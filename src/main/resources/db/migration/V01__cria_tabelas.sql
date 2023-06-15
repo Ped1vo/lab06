475fe8bd3d0b046c2b369904775217202d029518
Ped1vo <pedroivodl@gmail.com>
2023-06-15 17:49:18 -0300

    adicionar o flyway nas depedencias e fazer querys e criar banco e tabelas

--- src/main/resources/db/migration/V01__cria_tabelas.sql
+++ src/main/resources/db/migration/V01__cria_tabelas.sql
@@ -1,33 +1,37 @@
+DROP TABLE IF EXISTS tb_campeonato;
 CREATE TABLE IF NOT EXISTS tb_campeonato
 (
-    id_campeonato SERIAL PRIMARY KEY,
+    id_campeonato BIGINT PRIMARY KEY,
     ano           INTEGER      NOT NULL,
     nome          VARCHAR(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS tb_estadio;
 CREATE TABLE IF NOT EXISTS tb_estadio
 (
-    id_estadio   SERIAL PRIMARY KEY,
+    id_estadio   BIGINT PRIMARY KEY,
     nome         VARCHAR(50)  NOT NULL,
     endereco     VARCHAR(255) NOT NULL,
-    time_id_time BIGINT,
+    time_id_time BIGINT UNIQUE,
     FOREIGN KEY (time_id_time) REFERENCES tb_time (id_time)
 );
 
+DROP TABLE IF EXISTS tb_jogador;
 CREATE TABLE IF NOT EXISTS tb_jogador
 (
-    id_jogador   SERIAL PRIMARY KEY,
+    id_jogador   BIGINT PRIMARY KEY,
     nome         VARCHAR(50)      NOT NULL,
     nascimento   DATE             NOT NULL,
     genero       VARCHAR(255)     NOT NULL,
-    altura       DOUBLE PRECISION NOT NULL,
+    altura       FLOAT NOT NULL,
     time_id_time BIGINT,
     FOREIGN KEY (time_id_time) REFERENCES tb_time (id_time)
 );
 
+DROP TABLE IF EXISTS tb_partida;
 CREATE TABLE IF NOT EXISTS tb_partida
 (
-    id_partida               SERIAL PRIMARY KEY,
+    id_partida               BIGINT PRIMARY KEY,
     data                     DATE NOT NULL,
     time_id_time             BIGINT,
     campeonato_id_campeonato BIGINT,
@@ -35,10 +39,11 @@ CREATE TABLE IF NOT EXISTS tb_partida
     FOREIGN KEY (campeonato_id_campeonato) REFERENCES tb_campeonato (id_campeonato)
 );
 
+DROP TABLE IF EXISTS tb_time;
 CREATE TABLE IF NOT EXISTS tb_time
 (
-    id_time            SERIAL PRIMARY KEY,
+    id_time            BIGINT PRIMARY KEY,
     nome               VARCHAR(255) NOT NULL,
-    estadio_id_estadio BIGINT,
+    estadio_id_estadio BIGINT UNIQUE,
     FOREIGN KEY (estadio_id_estadio) REFERENCES tb_estadio (id_estadio)
 );
