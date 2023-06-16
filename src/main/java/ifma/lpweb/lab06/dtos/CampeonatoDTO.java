a92ddae2643e4e5a146c69fdab0384f2f90a1626
Ped1vo <pedroivodl@gmail.com>
2023-06-16 13:10:27 -0300

    começo de organizar o DTO

--- src/main/java/ifma/lpweb/lab06/dtos/CampeonatoDTO.java
+++ src/main/java/ifma/lpweb/lab06/dtos/CampeonatoDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class CampeonatoDTO {
+    private Long id;
     private String nome;
     private String endereco;
 }
--- src/main/java/ifma/lpweb/lab06/dtos/EstadioDTO.java
+++ src/main/java/ifma/lpweb/lab06/dtos/EstadioDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class EstadioDTO {
+    private Long id;
     private String nome;
     private String endereco;
 }
